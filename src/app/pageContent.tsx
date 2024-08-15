325e706373894cfb5f5fb9be617adda5ef374214
Daniel Kantor <git@daniel-kantor.com>
2024-08-15 19:38:33 +0200

    sort cards by icardsd

--- src/app/pageContent.tsx
+++ src/app/pageContent.tsx
@@ -3,15 +3,20 @@
 import { ExerciseCard } from '@/components/exercise-card';
 import { Button } from '@/components/ui/button';
 import { useCardStore } from './stores';
+import { useMemo } from 'react';
+import { sortBy } from 'lodash';
 
 export default function HomePageContent() {
   const { cards, addCard } = useCardStore();
+  const sortedCards = useMemo(() => {
+    return sortBy(cards, (card) => card.id);
+  }, [cards]);
 
   return (
     <main className="flex justify-center p-8">
       <Button onClick={addCard}>Add Card</Button>
       <div className="flex flex-col gap-4">
-        {cards.map((card) => (
+        {sortedCards.map((card) => (
           <ExerciseCard key={card.id} card={card} />
         ))}
       </div>
