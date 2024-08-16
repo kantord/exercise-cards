cfc2ec7c9673b015d1d01b29c642d43e227b8048
Daniel Kantor <git@daniel-kantor.com>
2024-08-16 13:15:41 +0200

    fix dev server

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+const debug = process.env.NODE_ENV !== 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /**
@@ -5,14 +7,14 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
+  output: 'export',
 
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/exercise-cards",
+  basePath: debug ? '' : '/exercise-cards',
 
   /**
    * Disable server-based image optimization. Next.js does not support
